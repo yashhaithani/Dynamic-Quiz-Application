20cad24e723994ab8e59353644b3b35bf6919d6b
YASH AITHANI <78597770+yashhaithani@users.noreply.github.com>
2024-03-26 12:18:53 +0530

    Delete QuizDao.java

--- QuizDao.java
+++
@@ -1,8 +0,0 @@
-package com.Aithani.QuizApp.dao;
-
-import com.Aithani.QuizApp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz,Integer>
-{
-}
